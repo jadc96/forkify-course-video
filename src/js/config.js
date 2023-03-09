28d7dd6e99c2385998e8723cacce556b9a8009b3
Jacques Antoine de Cugnac <ja.decugnac@outlook.com>
2023-03-09 11:15:34 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1ea91b4f-833e-4d8b-8c5d-f07d596b09a1';
+export const MODAL_CLOSE_SEC = 2.5;
